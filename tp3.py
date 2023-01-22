0708ec0d59a4a3baf8a29507ba44647c91de1d70
cornelia1504 <121952430+cornelia1504@users.noreply.github.com>
2023-01-22 19:59:08 +0100

    Update tp3.py

--- tp3.py
+++ tp3.py
@@ -1,18 +1,29 @@
 #encoding : utf8
+"""Program for creating class Animal that have some common attributes but can also have specific attributes. We can find all the descendant of an
+animal and find the parents of a child."""
+
 class Animal():
-    def set_species(self, species):
-        self.species = species
-    def __init__(self, species, age, diet) -> None :
+    def __init__(self, species, age,foot, diet, mother) -> None :
         self.species = species
         self.age = age
+        self.foot = foot
         self.diet = diet 
+        self.mother = mother
+        self.child_id = [] # contains not understandable id ##
+        self.children = [] # contains names
+        self.descendants = []
         
     def __str__(self) -> str:
-        return self.species + " " + str(self.age) + " " + str(self.diet) 
+        
+        return self.species + " Age: " + str(self.age) + "Nb foot: " + str(self.foot) + " Diet: " + str(self.diet)+ " " + str(self.mother) 
     
 class  Homme(Animal):
     def set_name(self, name):
         self.name = name 
+        
+    def __str__(self): #function to print info from Homme()
+        return super().__str__() + "\nname : " + self.name
+
     
 if __name__ == "__main__" :
     chat = Animal("chat", 3, "ommnivore")
