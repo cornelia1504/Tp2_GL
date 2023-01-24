f3bc76d758a422d45ef6d16e80742240f50ee87e
cornelia1504 <121952430+cornelia1504@users.noreply.github.com>
2023-01-24 01:20:11 +0100

    Update tp3.py

--- tp3.py
+++ tp3.py
@@ -1,38 +1,56 @@
 #encoding : utf8
-"""Program for creating class Animal that have some common attributes but can also have specific attributes. We can find all the descendant of an
-animal and find the parents of a child."""
-
+"""Program for creating class"""
 class Animal():
-    def __init__(self, species, age,foot, diet, mother) -> None :
+    """creating class Animal"""
+    def __init__(self, species, age, diet, mother) -> None :
+        """attributes = species, age, diet, mother"""
         self.species = species
         self.age = age
-        self.foot = foot
-        self.diet = diet 
+        self.diet = diet
         self.mother = mother
         self.child_id = [] # contains not understandable id ##
         self.children = [] # contains names
         self.descendants = []
-        
+
     def __str__(self) -> str:
-        
-        return self.species + " Age: " + str(self.age) + "Nb foot: " + str(self.foot) + " Diet: " + str(self.diet)+ " " + str(self.mother) 
-    
-class  Homme(Animal):
-    def set_name(self, name):
-        self.name = name 
-        
-    def __str__(self): #function to print info from Homme()
+        return "Species : " + self.species + "\n" \
+        +"age : " + str(self.age) + "\n" \
+        +"diet : "+ str(self.diet) + "\n"\
+        +"Mother : " + str(self.mother) + "\n"\
+        + "Children : " + str(self.children) + "\n"\
+        + "Children : " + str(self.children) + "\n"\
+        + "Descendent : " + str(self.descendants)
+
+class Cat(Animal):
+    """class for the species Homme decendant of Animal"""
+    def __init__(self, age , name) -> None:
+        """only name , age and diet attributes"""
+        super().__init__("Chat", age, "Omnivore", "animal")
+        self.age = age
+        self.name = name
+
+    def __str__(self):
+        """function to print info form Animal() and from Cat()"""
         return super().__str__() + "\nname : " + self.name
 
-    
-if __name__ == "__main__" :
-    chat = Animal("chat", 3, "ommnivore")
-    chat2 = Animal("chat", 3, "ommnivore")
-    animal2 = Homme("homme", 23, "omnivore")
-    animal2.set_name("Miora")
-print(animal2.name)
-print(Animal)
-print(chat)
-print(chat2)
+class Homme(Cat): #Homme descendant of cat
+    """class for the species Homme"""
+    def __init__(self,name, age) -> None:
+        """init what homme have 'homme' and 'omnivore" """
+        super().__init__("homme", age)
+        self.age = age
+        self.name = name
 
-#     animal1 = Animal()
+    def __str__(self):
+        """function to print info form Animal() and from Homme()"""
+        return super().__str__() + "\nname : " + self.name
+
+if __name__ == "__main__":
+    cat0 = Cat(10, "mimi")
+    cat1 = Cat(8, "mich")
+    homme0 = Homme(38, "Mitch")
+    print(cat0)
+    print(cat1)
+    print(homme0)
+
+#TO_DO :  function to add mother, child  and descendents on the lists
